a20251c0c19c2cd2c4a3402becbd5abd4bbdab99
Dylan Bickerstaff <dylan@belowaverage.org>
2019-10-07 21:54:14 -0400

    Took out unnessesary stuff

--- SuperGrateInstaller/precompile.ps1
+++ SuperGrateInstaller/precompile.ps1
@@ -1,7 +1,5 @@
 Remove-Item ".\bin\" -Force -Recurse
 New-Item ".\bin\scratch\x64\" -ItemType Directory -Force
 New-Item ".\bin\scratch\x86\" -ItemType Directory -Force
-Invoke-WebRequest -Uri "https://github.com/belowaverage-org/SuperGrate/raw/master/USMT/x64.zip" -OutFile ".\bin\scratch\x64\usmt.zip"
-Invoke-WebRequest -Uri "https://github.com/belowaverage-org/SuperGrate/raw/master/USMT/x86.zip" -OutFile ".\bin\scratch\x86\usmt.zip"
-Expand-Archive -Path ".\bin\scratch\x64\usmt.zip" -DestinationPath ".\bin\scratch\x64\usmt\" -Force
-Expand-Archive -Path ".\bin\scratch\x86\usmt.zip" -DestinationPath ".\bin\scratch\x86\usmt\" -Force
+Expand-Archive -Path "..\USMT\x64.zip" -DestinationPath ".\bin\scratch\x64\usmt\" -Force
+Expand-Archive -Path "..\USMT\x86.zip" -DestinationPath ".\bin\scratch\x86\usmt\" -Force
