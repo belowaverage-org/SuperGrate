11984c7d6da0c9c2e8d3b2c9062a039ae221cf63
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-12-12 16:10:22 -0500

    Test Commit

--- SuperGrate/Properties/AssemblyInfo.cs
+++ SuperGrate/Properties/AssemblyInfo.cs
@@ -21,17 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2f05c2fb-1f11-4d53-9d9d-c8cec72ab64e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("0.0.0.0")]
-//[assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: AssemblyVersion("1.2.1.0")]
 [assembly: AssemblyFileVersion("1.2.1.0")]
