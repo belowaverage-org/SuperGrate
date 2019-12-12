7833f8b77404e423b983df285a138d1622e620b0
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-12-12 16:13:36 -0500

    Test Commit - Pre Release

--- SuperGrate/Properties/AssemblyInfo.cs
+++ SuperGrate/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Super Grate - Remote User Migration & Backup")]
 [assembly: AssemblyDescription("Super Grate is a tool part of the Super Suite that can perform remote execution of Microsoft's USMT (User State Migration Tool) on any domain joined PC, or local execution on any non-joined PC.")]
 [assembly: AssemblyConfiguration("")]
@@ -13,12 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Dylan Bickerstaff")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2f05c2fb-1f11-4d53-9d9d-c8cec72ab64e")]
 
 [assembly: AssemblyVersion("1.2.1.0")]
