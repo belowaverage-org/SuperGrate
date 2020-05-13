4648921b344c9be2381f5af5c741b678aba634d3
Dylan Bickerstaff <dylan@belowaverage.org>
2020-05-13 13:26:50 -0400

    Removed installer, Updated Icon.

--- SuperGrateInstaller/precompile.ps1
+++
@@ -1,5 +0,0 @@
-Remove-Item ".\bin\" -Force -Recurse
-New-Item ".\bin\scratch\x64\" -ItemType Directory -Force
-New-Item ".\bin\scratch\x86\" -ItemType Directory -Force
-Expand-Archive -Path "..\USMT\x64.zip" -DestinationPath ".\bin\scratch\x64\usmt\" -Force
-Expand-Archive -Path "..\USMT\x86.zip" -DestinationPath ".\bin\scratch\x86\usmt\" -Force
