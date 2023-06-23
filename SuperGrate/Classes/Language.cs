8d4fdbe53d69753de0dea040de91bde42961abba
Dylan Bickerstaff <dylan@belowaverage.org>
2023-06-23 00:26:16 -0400

    Commented language class.

--- SuperGrate/Classes/Language.cs
+++ SuperGrate/Classes/Language.cs
@@ -8,9 +8,21 @@ namespace SuperGrate.Classes
 {
     internal static class Language
     {
+        /// <summary>
+        /// The default language type Super Grate uses if the specified language isn't found.
+        /// </summary>
         private const string DefaultLanguage = "en-US";
+        /// <summary>
+        /// A list of SGLanguage XML documents that is loaded into memory.
+        /// </summary>
         private static Dictionary<string, XmlDocument> LanguageDocuments = new Dictionary<string, XmlDocument>();
+        /// <summary>
+        /// The current executing assembly object.
+        /// </summary>
         private static Assembly Assembly = Assembly.GetExecutingAssembly();
+        /// <summary>
+        /// The currently selected language. The config file is first priority, second is the system culture, the third is the default language constant.
+        /// </summary>
         public static string SelectedLanguage 
         {
             get
@@ -20,6 +32,10 @@ namespace SuperGrate.Classes
                 return DefaultLanguage;
             }
         }
+        /// <summary>
+        /// Load a specified language into the LanguageDocuments dictionary.
+        /// </summary>
+        /// <param name="Language">Language Code, example: en-US.</param>
         public static void LoadLanguage(string Language)
         {
             try
@@ -33,6 +49,22 @@ namespace SuperGrate.Classes
                 Logger.Exception(e, "Failed to find language.");
             }
         }
+        /// <summary>
+        /// Get a string via the specified language, key and optionally replacement values.
+        /// </summary>
+        /// <param name="Language">Language Code, example: en-US.</param>
+        /// <param name="Key">A string key code. This specifies which string to retrieve from the language file.</param>
+        /// <param name="Replacements">
+        ///     A list of strings in sequential order to replace macros in the language file string.
+        ///     For instance, the first replacement value will replace "{0}" in the string from the XML language file, and
+        ///     the second string defined will replace "{1}".
+        /// </param>
+        /// <returns>
+        ///     A string from the specified language code and string key code with the optional replacements performed.
+        ///     If a specified language is not found, the default language will be used.
+        ///     If the specified language is found, but the string key code is not found in that language XML, the default language will be used.
+        ///     On all other errors, this method will return "???".
+        /// </returns>
         public static string GetWithLanguage(string Language, string Key, params string[] Replacements)
         {
             if (!LanguageDocuments.ContainsKey(Language)) LoadLanguage(Language);
@@ -50,10 +82,29 @@ namespace SuperGrate.Classes
             }
             return text;
         }
+        /// <summary>
+        /// Get a string via the specified key code and optionally replacement values.
+        /// </summary>
+        /// <param name="Key">A string key code. This specifies which string to retrieve from the language file.</param>
+        /// <param name="Replacements">
+        ///     A list of strings in sequential order to replace macros in the language file string.
+        ///     For instance, the first replacement value will replace "{0}" in the string from the XML language file, and
+        ///     the second string defined will replace "{1}".
+        /// </param>
+        /// <returns>
+        ///     A string from the specified string key code with the optional replacements performed.
+        ///     If the string key code is not found in the language XML, the default language will be used.
+        ///     On all other errors, this method will return "???".
+        /// </returns>
         public static string Get(string Key, params string[] Replacements)
         {
             return GetWithLanguage(SelectedLanguage, Key, Replacements);
         }
+        /// <summary>
+        /// Check if the specified language code is defined in the compiled assembly.
+        /// </summary>
+        /// <param name="Language">Language Code, example: en-US.</param>
+        /// <returns>True if language is found, false if not.</returns>
         private static bool LanguageIsDefined(string Language)
         {
             return Assembly.GetManifestResourceNames().Contains("SuperGrate.Localization." + Language + ".xml");
