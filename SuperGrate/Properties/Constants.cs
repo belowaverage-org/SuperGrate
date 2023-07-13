e3be34c430187f7490d5f9660b951dec01d97966
BrewCityGeek <brewcitygeek@gmail.com>
2023-07-13 15:27:59 -0500

    Update Constants.cs
    Updating URL to point to my own Repo

--- SuperGrate/Properties/Constants.cs
+++ SuperGrate/Properties/Constants.cs
@@ -5,10 +5,10 @@
         /// <summary>
         /// Download location for USMT ZIP File (X64).
         /// </summary>
-        public const string USMTx64URL = "https://github.com/belowaverage-org/SuperGrate/raw/master/USMT/x64.zip";
+        public const string USMTx64URL = "https://github.com/BrewCityGeek/SuperGrate/raw/master/USMT/x64.zip";
         /// <summary>
         /// Download location for USMT ZIP File (X86).
         /// </summary>
-        public const string USMTx86URL = "https://github.com/belowaverage-org/SuperGrate/raw/master/USMT/x86.zip";
+        public const string USMTx86URL = "https://github.com/BrewCityGeek/SuperGrate/raw/master/USMT/x86.zip";
     }
 }
