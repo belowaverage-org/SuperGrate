56692734fd8160c4fbb496be7a59397be9954e6f
Dylan Bickerstaff <dylan@belowaverage.org>
2023-08-22 15:09:57 -0400

    Update UserList.cs

--- SuperGrate/Classes/UserList.cs
+++ SuperGrate/Classes/UserList.cs
@@ -96,8 +96,8 @@ namespace SuperGrate.UserList
             {
                 Rows.Sort(delegate (UserRow x, UserRow y) {
                     if (!x.ContainsKey(SortColumn) && !y.ContainsKey(SortColumn)) return 0;
-                    if (!x.ContainsKey(SortColumn) || x[SortColumn] == null) return -1;
-                    if (!y.ContainsKey(SortColumn) || y[SortColumn] == null) return 1;
+                    if (!x.ContainsKey(SortColumn) || x[SortColumn] == "" || x[SortColumn] == null) return -1;
+                    if (!y.ContainsKey(SortColumn) || y[SortColumn] == "" || y[SortColumn] == null) return 1;
                     if (x[SortColumn] == y[SortColumn]) return 0;
                     if (long.TryParse(x[SortColumn], out long intX) && long.TryParse(y[SortColumn], out long intY))
                     {
