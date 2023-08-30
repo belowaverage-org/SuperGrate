1e2f00be513b331e5cdab13894b22135ef8f413d
Dylan Bickerstaff <dylan@belowaverage.org>
2023-08-30 03:28:52 -0400

    Translated remaining language.

--- SuperGrate/Classes/USMT.cs
+++ SuperGrate/Classes/USMT.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
